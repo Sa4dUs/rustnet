04ccb7adbb9b4d2191b41601843e8bc1b31428c4
Lamoara <jorginos40@gmail.com>
2024-06-02 17:13:30 +0200

    data normalization

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let inputs = 0..20;
     let outputs = 20..21;
 
-    let mut neural_network = NeuralNetwork::new(&vec![(inputs.len(), SIGMOID), (8, SIGMOID), (4, SIGMOID),(outputs.len(), SIGMOID)]);
+    let mut neural_network = NeuralNetwork::new(&vec![(inputs.len(), SIGMOID), (4, SIGMOID), (outputs.len(), RELU)]);
 
     let csv_out = read_csv_to_neural_input("phone_price_train.csv", &inputs, &outputs).expect("csv reading failed");
 
@@ -24,7 +24,7 @@ fn main() {
     x_train = csv_out[0].clone();
     y_train = csv_out[1].clone();
 
-    let csv_out = read_csv_to_neural_input("phone_price_test.csv", &inputs, &outputs).expect("csv reading failed");
+    let csv_out = read_csv_to_neural_input("phone_price_train.csv", &inputs, &outputs).expect("csv reading failed");
 
     let mut x_test = csv_out[0].clone();;
     let mut y_test = csv_out[1].clone();;
@@ -33,5 +33,5 @@ fn main() {
 
     neural_network.train(x_train.clone(), y_train.clone(), MSE, learning_rate);
 
-    neural_network.test(x_test, y_test, MSE, 0.1);
+    neural_network.test(x_test, y_test, MSE, 0.3);
 }
