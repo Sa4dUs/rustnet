7c23667d19056b981c0f09e5cfea8a691fe131d7
Lamoara <jorginos40@gmail.com>
2024-06-03 20:19:40 +0200

    datasets added

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,33 @@
 use std::collections::VecDeque;
 use ndarray::{Array1, Array2};
+use ndarray_rand::rand_distr::num_traits::signum;
 use neural_network::lib::csv_loader::read_csv_to_neural_input;
 
 use rand::rngs::StdRng;
 use rand::Rng;
 use rand::SeedableRng;
-use neural_network::lib::math::{MSE, probability_density_function, RELU};
+use neural_network::lib::math::{MSE, probability_density_function, RELU, SIGMOID};
 use neural_network::neural::network::NeuralNetwork;
 
 fn main() {
-    let inputs = 0..3;
-    let outputs = 3..4;
+    let inputs = 0..8;
+    let outputs = 8..9;
 
-    let csv_out = read_csv_to_neural_input("train.csv", &inputs, &outputs).expect("csv reading failed");
+    let csv_out = read_csv_to_neural_input("mushroom_cleaned.csv", &inputs, &outputs).expect("csv reading failed");
     let x_train = csv_out[0].clone();
     let y_train = csv_out[1].clone();
 
-    let csv_out = read_csv_to_neural_input("test.csv", &inputs, &outputs).expect("csv reading failed");
+    let csv_out = read_csv_to_neural_input("mushroom_cleaned.csv", &inputs, &outputs).expect("csv reading failed");
     let x_test: Vec<Array2<f64>> = csv_out[0].clone();
     let y_test: Vec<Array2<f64>> = csv_out[1].clone();
 
     let mut rng = StdRng::seed_from_u64(1);
 
-    let mut nn = NeuralNetwork::new(inputs.len(), vec![(4, RELU), (1, RELU)], &mut rng);
+    let mut nn = NeuralNetwork::new(inputs.len(), vec![(6, SIGMOID), (4, SIGMOID), (outputs.len(), SIGMOID)], &mut rng);
 
     let loss_f = MSE;
-    let learning_rate = 0.05;
-    let epochs = 1000;
+    let learning_rate = 0.03;
+    let epochs = 10;
     let threshold = 0.05;
 
     nn.train(&x_train, &y_train, learning_rate, loss_f, epochs);
