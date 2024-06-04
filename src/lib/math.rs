5addb65a39a8cd29c97c040b8fbea7c565ad4f3b
Marcelo Domínguez <69964857+Sa4dUs@users.noreply.github.com>
2024-06-04 16:02:51 +0200

    didn't commit tonight

--- src/lib/math.rs
+++ src/lib/math.rs
@@ -1,5 +1,5 @@
 use std::iter::Iterator;
-use ndarray::Array2;
+use ndarray::{Array2, Axis};
 use ndarray_rand::rand_distr::num_traits::Pow;
 use ndarray_rand::rand_distr::num_traits::real::Real;
 
@@ -13,6 +13,33 @@ pub const RELU: ActivationFunction = (|t| t.mapv(|e| f64::max(0.0, e)), |t| t.ma
 
 pub const MSE: ErrorFunction = (|x, y| (x - y).mapv(|x| x * x).mean().expect("MSE Error"), |x, y| 2.0 * (x.clone() - y) / x.len() as f64);
 pub const CROSS_ENTROPY: ErrorFunction = (|x, y| (-1.0/x.len() as f64)*(x.iter().zip(y).map(|(xi, yi)| yi*(xi.ln())+(1.0-yi)*((1.0-xi).ln())).sum::<f64>()), |x, y| y.clone()/x.clone() + (1.0-y)/(1.0-x));
+pub const SOFTMAX_CROSS_ENTROPY: ErrorFunction = (
+    |x: Array2<f64>, y: Array2<f64>| -> f64 {
+        let softmax = |row: &Array2<f64>| -> Array2<f64> {
+            let max_x = row.map_axis(Axis(1), |x| *x.iter().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap());
+            let exp_x = row - &max_x;
+            let exp_x = exp_x.mapv(|xi| xi.exp());
+            let sum_exp_x = exp_x.sum_axis(Axis(1)).insert_axis(Axis(1));
+            exp_x / sum_exp_x
+        };
+
+        let probs = softmax(&x);
+        let log_likelihood = y * &probs.mapv(f64::ln);
+        -log_likelihood.sum() / x.nrows() as f64
+    },
+    |x: Array2<f64>, y: Array2<f64>| -> Array2<f64> {
+        let softmax = |row: &Array2<f64>| -> Array2<f64> {
+            let max_x = row.map_axis(Axis(1), |x| *x.iter().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap());
+            let exp_x = row - &max_x;
+            let exp_x = exp_x.mapv(|xi| xi.exp());
+            let sum_exp_x = exp_x.sum_axis(Axis(1)).insert_axis(Axis(1));
+            exp_x / sum_exp_x
+        };
+
+        let probs = softmax(&x);
+        (probs - y) / x.nrows() as f64
+    }
+);
 
 pub fn mean(a: &Array2<f64>) -> f64 {
     a.sum() / (a.len() as f64)
